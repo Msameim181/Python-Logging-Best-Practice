d489796b22c958f778a044888e0c7b51186dc5b8
Mohammad Mahdi Samei <9259samei@gmail.com>
2024-11-30 21:57:21 +0330

    feat: Refactor imports in __init__.py for Django and FastAPI RequestIdMiddleware

--- src/chromatrace/__init__.py
+++ src/chromatrace/__init__.py
@@ -1,4 +1,10 @@
-from .fastapi import RequestIdMiddleware  # noqa
+from .django import RequestIdMiddleware as DjangoRequestIdMiddleware  # noqa
+from .fastapi import RequestIdMiddleware as FastAPIRequestIdMiddleware  # noqa
 from .logging_config import LoggingConfig  # noqa
 from .logging_settings import LoggingSettings  # noqa
-from .tracer import RequestIdContext, trace_id_ctx, tracer  # noqa
+from .tracer import (  # noqa
+    RequestIdContext,
+    get_trace_id,
+    trace_id_ctx,
+    tracer,
+)
