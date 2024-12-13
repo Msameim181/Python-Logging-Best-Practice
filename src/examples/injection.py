238c4f0358a8f83bf3d72cbef764a0018e1ef2d1
Mohammad Mahdi Samei <9259samei@gmail.com>
2024-12-13 21:02:49 +0330

    refactor(injection): enable NaN trace ignoring and process ID display in logging settings

--- src/examples/injection.py
+++ src/examples/injection.py
@@ -7,8 +7,9 @@ from lagom import Singleton
 container[LoggingSettings] = LoggingSettings(
     application_level="Development",
     enable_tracing=True,
-    ignore_nan_trace=False,
+    ignore_nan_trace=True,
     enable_file_logging=True,
+    show_process_id=True,
 )
 container[APIService] = Singleton(APIService)
 container[SocketService] = Singleton(SocketService)
